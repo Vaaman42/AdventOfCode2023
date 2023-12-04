1a55e933393044a01e65daa9d6acaa47c8c28281
Aurélien Dekesel <aurelien.dekesel@gmail.com>
2023-12-04 10:21:19 +0100

    Day2

--- Program.cs
+++ Program.cs
@@ -9,10 +9,72 @@ while (true)
         case "1":
             Day1();
             break;
+        case "2":
+            Day2();
+            break;
         default:
             Console.WriteLine("Error, try again.");
             break;
     }
+}
+
+void Day2()
+{
+    Console.WriteLine("Enter input\nEnter \"END\" when finished.");
+    var input = Console.ReadLine();
+    var idSum = 0;
+    int powerSum = 0;
+    int maxRed = 12, maxGreen = 13, maxBlue = 14;
+    do
+    {
+        // Id is the first number found
+        var gameSplit = input!.Split(": ");
+        var id = int.Parse(gameSplit[0].Split(' ')[1]);
+        var sets = gameSplit[1].Split("; ");
+        bool isRoundPossible = true;
+        int minRed = 0, minGreen = 0, minBlue = 0;
+        foreach (var set in sets)
+        {
+            var dices = set.Split(", ");
+            foreach (var dice in dices)
+            {
+                var split = dice.Split(' ');
+                var count = int.Parse(split[0]);
+                switch (split[1])
+                {
+                    case "red":
+                        if (count > maxRed)
+                            isRoundPossible = false;
+                        if (count > minRed)
+                            minRed = count;
+                        break;
+                    case "green":
+                        if (count > maxGreen)
+                            isRoundPossible = false;
+                        if (count > minGreen)
+                            minGreen = count;
+                        break;
+                    case "blue":
+                        if (count > maxBlue)
+                            isRoundPossible = false;
+                        if (count > minBlue)
+                            minBlue = count;
+                        break;
+                }
+            }
+        }
+
+        if (isRoundPossible)
+            idSum += id;
+        
+        var power = minRed * minGreen * minBlue;
+        powerSum += power;
+
+        input = Console.ReadLine();
+    } while (input != "END");
+    
+    Console.WriteLine("PART1: " + idSum);
+    Console.WriteLine("PART2: " + powerSum);
 }
 
 void Day1()
